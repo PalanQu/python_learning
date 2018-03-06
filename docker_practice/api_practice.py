ce401a192bee85c893692162728e332550a81a83
PalanQu <palan_work@163.com>
2018-03-06 13:34:22 +0800

    fix bugs in pr

--- docker_practice/api_practice.py
+++ docker_practice/api_practice.py
@@ -2,98 +2,84 @@ import os
 import requests
 
 def check_connection(daemon_url="http://127.0.0.1:2376"):
-    '''测试与docker的连接是否已经建立成功'''
+    """测试与docker的连接是否已经建立成功"""
     check_connection_url = os.path.join(daemon_url, "containers/json")
     params = {"all": 1}
     r = requests.get(url=check_connection_url, params=params)
-    return [r.status_code == 200, r.text]
+    return r.status_code == 200, r.text
 
 
 def create_container(
-        container_name= "test-container", *,
+        container_name, *,
         daemon_url="http://127.0.0.1:2376",
         image="ubuntu"):
-    '''建立一个新的container'''
-    params = {
-        "create_container_url": os.path.join(daemon_url, "containers/create"),
-        "request_data": {"Image": image},
-        "request_header": {"Content-Type": "application/json"},
-        "request_params": {"name": container_name}
-    }
+    """建立一个新的container"""
     r = requests.post(
-        url=params["create_container_url"],
-        params=params["request_params"],
-        json=params["request_data"],
-        headers=params["request_header"])
-    return [r.status_code == 201, r.text]
+        url=os.path.join(daemon_url, "containers/create"),
+        params={"name": container_name},
+        json={"Image": image},
+        headers={"Content-Type": "application/json"})
+    return r.status_code == 201, r.text
 
 
 def commit_container(
-        container_name="ubuntu-test", *,
+        container_name, *,
         daemon_url="http://127.0.0.1:2376",
         comment="my comments"):
-    '''提交对container的修改'''
-    params = {
-        "commit_container_url": os.path.join(daemon_url, "commit"),
-        "request_header": {"Content-Type": "application/json"},
-        "request_paras": {"container": container_name, "comment": comment}
-    }
+    """提交对container的修改"""
     r = requests.post(
-        url=params["commit_container_url"],
-        params=params["request_paras"],
-        headers=params["request_header"])
-    return [r.status_code == 201, r.text]
+        url=os.path.join(daemon_url, "commit"),
+        params={
+            "container": container_name,
+            "comment": comment
+        },
+        headers={"Content-Type": "application/json"})
+    return r.status_code == 201, r.text
 
 
 def push_modified_commit(
         repo_name="palanqu/docker_learning", *,
         daemon_url="http://127.0.0.1:2376",
         registry_auth):
-    '''将commit提交到repo上'''
-    params = {
-        "push_modified_commit_url":
-            os.path.join(daemon_url, "images", repo_name, "push"),
-        "request_header": {"X-Registry-Auth": registry_auth}
-    }
+    """将commit提交到repo上"""
     r = requests.post(
-        url=params["push_modified_commit_url"],
-        headers=params["request_header"])
-    return [r.status_code == 200, r.text]
+        url=os.path.join(daemon_url, "images", repo_name, "push"),
+        headers={"X-Registry-Auth": registry_auth})
+    return r.status_code == 200, r.text
 
 
 if __name__ == "__main__":
-    params = {
-        "daemon_url": "http://127.0.0.1:2376",
-        "image": "ubuntu",
-        "container_name": "ubuntu-container",
-        "registry_auth": "eyJ1c2VybmFtZSI6InBhbGFucXUiLCJwYXNzd29yZCI6Ik1ESjAyMTQ4MDQxOTMtKyIsICJhdXRoIjoiIiwiZW1haWwiOiJwYWxhbl93b3JrQDE2My5jb20ifQ",
-        "repo_name": "palanqu/docker_learning",
-        "comment": "commit test"
-    }
+    daemon_url = "http://127.0.0.1:2376"
+    image = "ubuntu"
+    container_name =  "ubuntu-container"
+    registry_auth = "eyJ1c2VybmFtZSI6InBhbGFucXUiLCJwYXNzd29yZCI6Ik1ESjAyMTQ4MDQxOTMtKyIsICJhdXRoIjoiIiwiZW1haWwiOiJwYWxhbl93b3JrQDE2My5jb20ifQ"
+    repo_name = "palanqu/docker_learning"
+    comment = "commit test"
+
     if_connection_success, check_connection_response_text = \
-        check_connection(params["daemon_url"])
+        check_connection(daemon_url)
 
     if if_connection_success:
         print("checked the connection")
         if_create_container_success, create_container_response_text = \
             create_container(
-                params["container_name"],
-                daemon_url=params["daemon_url"],
-                image=params["image"])
+                container_name,
+                daemon_url=daemon_url,
+                image=image)
         if if_create_container_success:
             print("create container success, container name: ",
-                  params["container_name"])
+                  container_name)
             if_commit_container_success, commit_container_response_text = \
                 commit_container(
-                    params["container_name"],
-                    daemon_url=params["daemon_url"],
-                    comment=params["comment"])
+                    container_name,
+                    daemon_url=daemon_url,
+                    comment=comment)
             if if_commit_container_success:
                 print("commit container success")
                 if_push_modified_commit_success, push_modified_commit_response_text = \
-                    push_modified_commit(params["repo_name"],
-                                         daemon_url=params["daemon_url"],
-                                         registry_auth=params["registry_auth"])
+                    push_modified_commit(repo_name,
+                                         daemon_url=daemon_url,
+                                         registry_auth=registry_auth)
                 if if_push_modified_commit_success:
                     print("push modified commit success")
                 else:
