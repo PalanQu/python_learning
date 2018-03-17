82b847aabab240f850e09ee573cc993e58fa9252
PalanQu <palan_work@163.com>
2018-03-17 20:49:04 +0800

    add kubernetes practice

---
+++ kubernetes_practice/resources/server.py
@@ -0,0 +1,34 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+"""
+This is a sample http server
+"""
+
+class Request_handler(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(bytes("<p>hello</p>", "utf-8"))
+
+    def do_HEAD(self):
+        self._set_headers()
+
+
+def run(server_class=HTTPServer, handler_class=Request_handler, port=8088):
+    server_address = ('localhost', port)
+    httpd = server_class(server_address, handler_class)
+    print('Starting httpd...')
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
