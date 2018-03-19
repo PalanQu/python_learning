6296f14126741fbc92b0d6d13ad8eb6a54a645ab
PalanQu <palan_work@163.com>
2018-03-19 15:53:11 +0800

    replace global variables to capital

--- docker_practice/api_practice.py
+++ docker_practice/api_practice.py
@@ -1,8 +1,8 @@
 import os
 import requests
 
-daemon_url = "http://127.0.0.1:2376"
-def check_connection(daemon_url=daemon_url):
+DAEMON_URL = "http://127.0.0.1:2376"
+def check_connection(daemon_url=DAEMON_URL):
     """测试与docker的连接是否已经建立成功"""
     check_connection_url = os.path.join(daemon_url, "containers/json")
     params = {"all": 1}
@@ -12,7 +12,7 @@ def check_connection(daemon_url=daemon_url):
 
 def create_container(
         container_name, *,
-        daemon_url=daemon_url,
+        daemon_url=DAEMON_URL,
         image="ubuntu"):
     """建立一个新的container"""
     r = requests.post(
@@ -25,7 +25,7 @@ def create_container(
 
 def commit_container(
         container_name, *,
-        daemon_url=daemon_url,
+        daemon_url=DAEMON_URL,
         comment="my comments"):
     """提交对container的修改"""
     r = requests.post(
@@ -40,7 +40,7 @@ def commit_container(
 
 def push_modified_commit(
         repo_name="palanqu/docker_learning", *,
-        daemon_url=daemon_url,
+        daemon_url=DAEMON_URL,
         registry_auth):
     """将commit提交到repo上"""
     r = requests.post(
@@ -57,14 +57,14 @@ if __name__ == "__main__":
     comment = "commit test"
 
     if_connection_success, check_connection_response_text = \
-        check_connection(daemon_url)
+        check_connection(DAEMON_URL)
 
     if if_connection_success:
         print("checked the connection")
         if_create_container_success, create_container_response_text = \
             create_container(
                 container_name,
-                daemon_url=daemon_url,
+                daemon_url=DAEMON_URL,
                 image=image)
         if if_create_container_success:
             print("create container success, container name: ",
@@ -72,13 +72,13 @@ if __name__ == "__main__":
             if_commit_container_success, commit_container_response_text = \
                 commit_container(
                     container_name,
-                    daemon_url=daemon_url,
+                    daemon_url=DAEMON_URL,
                     comment=comment)
             if if_commit_container_success:
                 print("commit container success")
                 if_push_modified_commit_success, push_modified_commit_response_text = \
                     push_modified_commit(repo_name,
-                                         daemon_url=daemon_url,
+                                         daemon_url=DAEMON_URL,
                                          registry_auth=registry_auth)
                 if if_push_modified_commit_success:
                     print("push modified commit success")
